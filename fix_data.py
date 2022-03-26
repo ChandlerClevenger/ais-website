6fee872c32b5892882ce2f5027d83a655e72a338
Chandler Clevenger <Chandler888@icloud.com>
2022-03-26 18:28:34 -0400

    add zooming functionalities

--- fix_data.py
+++ fix_data.py
@@ -1,8 +1,16 @@
 
-with open('.\denmark_tiles\MAP_VIEW.json', 'r') as istr:
-    with open('.\denmark_tiles\MAP_VIEW_FIXED.json', 'w') as ostr:
-        ostr.write("[")
-        for line in istr:
-            line = line.rstrip('\n') + ','
-            print(line, file=ostr)
-        ostr.write("]")
+# with open('.\denmark_tiles\MAP_VIEW.json', 'r') as istr:
+#     with open('.\denmark_tiles\MAP_VIEW_FIXED.json', 'w') as ostr:
+#         ostr.write("[")
+#         for line in istr:
+#             line = line.rstrip('\n') + ','
+#             print(line, file=ostr)
+#         ostr.write("]")
+
+# from PIL import Image
+# images = ['43F9.png', '38F7.png', '39F7.png', '40F7.png', '41F7.png', '42F7.png', '43F7.png', '38F8.png', '39F8.png', '40F8.png', '41F8.png', '42F8.png', '43F8.png', '38F9.png', '39F9.png', '40F9.png', '38G0.png', '39G0.png', '40G0.png', '41G0.png', '42G0.png', '43G0.png', '38G1.png', '39G1.png', '40G1.png', '41G1.png', '42G1.png', '43G1.png', '38G2.png', '39G2.png', '40G2.png', '41G2.png', '42G2.png', '43G2.png']
+
+# for image in images :
+#     image = Image.open('./denmark_tiles/'+ image)
+#     image.show()
+
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 let tileDiv = document.querySelector("#tileDisplay");
-let clickedTiles = [];
+let lastClickedTile;
 let containedTiles = [];
-let currentTileICESName = "-1";
 let currentTileJSON;
 let tileJSON;
 
@@ -11,111 +10,91 @@ $.ajax({
   dataType: "json",
   success: function (data) {
     tileJSON = data;
-    updateContainedTiles(currentTileICESName);
+    displayDefault();
   },
   fail: function (err) {
     console.log(err);
   },
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-  let rootImg = document.createElement("img");
-  rootImg.setAttribute("src", "./denmark_tiles/ROOT.png");
-  rootImg.addEventListener("click", (event) => {
-    let x = event.clientX - tileDiv.getBoundingClientRect().x;
-    let y = event.clientY - tileDiv.getBoundingClientRect().y;
-    currentTileJSON = getTileFromJSON(currentTileICESName);
-    let minLat = currentTileJSON.image_north;
-    let maxLat = currentTileJSON.image_south;
-    let maxLon = currentTileJSON.image_east;
-    let minLon = currentTileJSON.image_west;
-    console.log(
-      "minLat: ",
-      minLat,
-      "minLon:",
-      minLon,
-      "maxLat:",
-      maxLat,
-      "maxLon:",
-      maxLon
-    );
-    let clickedLat = convertVal(
-      tileDiv.getBoundingClientRect().height,
-      0,
-      maxLat,
-      minLat,
-      y
-    );
-
-    let clickedLon = convertVal(
-      tileDiv.getBoundingClientRect().width,
-      0,
-      maxLon,
-      minLon,
-      x
-    );
-
-    console.log("ClickedLon:", clickedLon);
-    console.log("ClickedLat:", clickedLat);
-    console.log("x:", x, "y:", y);
-    currentTileJSON = findClickedTile(clickedLon, clickedLat);
-    console.log(currentTileJSON);
-    updateDisplay(currentTileJSON);
-  });
-  tileDiv.appendChild(rootImg);
+$("#tileDisplay").on("DOMSubtreeModified", () => {
+  if (currentTileJSON && currentTileJSON.scale == 2) {
+    lastClickedTile = currentTileJSON;
+  }
+  currentTileJSON = getTileFromTileId(tileDiv.getAttribute("data-tileid"));
+  updateContainedTiles(currentTileJSON.id);
 });
 
-function updateDisplay(currentTileJSON) {
-  let img = document.createElement("img");
-  img.setAttribute("src", "./denmark_tiles/" + currentTileJSON.filename);
-  img.addEventListener("click", (event) => {
-    let x = event.clientX - tileDiv.getBoundingClientRect().x;
-    let y = event.clientY - tileDiv.getBoundingClientRect().y;
-    currentTileJSON = getTileFromJSON(currentTileICESName);
-    let maxLat = currentTileJSON.image_north;
-    let maxLon = currentTileJSON.image_west;
-    let minLat = currentTileJSON.image_south;
-    let minLon = currentTileJSON.image_east;
+tileDiv.addEventListener("contextmenu", (event) => {
+  event.preventDefault();
+});
 
-    let clickedLat = convertVal(
-      tileDiv.getBoundingClientRect().height,
-      0,
-      maxLat,
-      minLat,
-      y
-    );
+function displayDefault() {
+  currentTileJSON = getTileFromTileId("1");
+  updateDisplay(currentTileJSON);
+}
 
-    let clickedLon = convertVal(
-      tileDiv.getBoundingClientRect().width,
-      0,
-      maxLon,
-      minLon,
-      x
-    );
+function handleClick(event) {
+  if (event.button == 0) {
+    leftclick(event);
+  } else if (event.button == 2) {
+    switch (currentTileJSON.scale) {
+      case 1:
+        break;
+      case 3:
+        updateDisplay(lastClickedTile);
+        break;
+      default:
+        displayDefault();
+        break;
+    }
+  }
+  updateDisplay(currentTileJSON);
+}
 
-    console.log("ClickedLon:", clickedLon);
-    console.log("ClickedLat:", clickedLat);
-    console.log("x:", x, "y:", y);
-    currentTileJSON = findClickedTile(clickedLon, clickedLat);
-    updateDisplay(currentTileJSON);
-  });
+function leftclick(event) {
+  if (currentTileJSON && currentTileJSON.scale == 3) return;
+  let x = event.clientX - tileDiv.getBoundingClientRect().x;
+  let y = event.clientY - tileDiv.getBoundingClientRect().y;
+  let minLat = currentTileJSON.image_north;
+  let maxLat = currentTileJSON.image_south;
+  let maxLon = currentTileJSON.image_east;
+  let minLon = currentTileJSON.image_west;
+  let clickedLat = convertVal(
+    tileDiv.getBoundingClientRect().height,
+    0,
+    maxLat,
+    minLat,
+    y
+  );
+
+  let clickedLon = convertVal(
+    tileDiv.getBoundingClientRect().width,
+    0,
+    maxLon,
+    minLon,
+    x
+  );
+
+  let clickedTileJSON = findClickedTile(clickedLon, clickedLat);
+  if (clickedTileJSON == 0) {
+    displayDefault();
+    return;
+  }
+  updateDisplay(clickedTileJSON);
+}
+
+function updateDisplay(tileJSON) {
+  tileDiv.setAttribute("data-tileId", tileJSON.id);
   tileDiv.innerHTML = "";
+  let img = document.createElement("img");
+  img.setAttribute("src", "./denmark_tiles/" + tileJSON.filename);
+  img.addEventListener("mouseup", (event) => handleClick(event));
   tileDiv.appendChild(img);
 }
 
 function findClickedTile(clickedLon, clickedLat) {
   for (let tile of containedTiles) {
-    console.log(tile.filename);
-    console.log(
-      "south:",
-      tile.south,
-      "east:",
-      tile.east,
-      "west:",
-      tile.west,
-      "north:",
-      tile.north
-    );
     if (
       tile.south <= clickedLat &&
       tile.north >= clickedLat &&
@@ -132,51 +111,23 @@ function convertVal(oldMax, oldMin, newMax, newMin, oldValue) {
   return ((oldValue - oldMin) * (newMax - newMin)) / (oldMax - oldMin) + newMin;
 }
 
-function getTileFromJSON(ICESName) {
+function getTileFromTileId(tileId) {
   for (let obj of Object.keys(tileJSON)) {
-    if (tileJSON[obj].ICESName == ICESName) {
+    if (tileJSON[obj].id == tileId) {
       return tileJSON[obj];
     }
   }
   return 0;
 }
 
-function updateContainedTiles(containedByICESName) {
+function updateContainedTiles(containerId) {
   containedTiles = [];
+  if (containerId == 1) {
+    containerId = -1;
+  }
   for (let obj of Object.keys(tileJSON)) {
-    if (tileJSON[obj].contained_by == containedByICESName) {
+    if (tileJSON[obj].contained_by == containerId) {
       containedTiles.push(tileJSON[obj]);
     }
   }
-  logContainedTiles();
-}
-
-function logContainedTiles() {
-  console.log("Sizing", containedTiles[0].scale);
-  console.log("# contained", containedTiles.length, containedTiles);
-}
-
-function printMins() {
-  let minLat = tileJSON[0].east;
-  let maxLat = tileJSON[0].west;
-  let minLon = tileJSON[0].north;
-  let maxLon = tileJSON[0].south;
-  for (let tile of Object.keys(tileJSON)) {
-    if (minLat > tileJSON[tile].east) {
-      minLat = tileJSON[tile].east;
-    }
-    if (maxLat < tileJSON[tile].west) {
-      maxLat = tileJSON[tile].west;
-    }
-    if (maxLon < tileJSON[tile].south) {
-      maxLon = tileJSON[tile].south;
-    }
-    if (minLon > tileJSON[tile].north) {
-      minLon = tileJSON[tile].north;
-    }
-  }
-  console.log("MinLat: ", minLat, "\n");
-  console.log("MinLon: ", minLon, "\n");
-  console.log("MaxLat: ", maxLat, "\n");
-  console.log("MaxLon: ", maxLon, "\n");
 }
