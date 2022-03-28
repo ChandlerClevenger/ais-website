e0f32639d3bd0b819d3e57c7fbc6e946209ab03c
Chandler Clevenger <Chandler888@icloud.com>
2022-03-28 11:28:01 -0400

    handle GET versus POST and responding to client

--- web_service/server.js
+++ web_service/server.js
@@ -11,32 +11,52 @@ const headers = {
 
 http
   .createServer((req, res) => {
-    let builder = new StringDecoder("utf-8");
-    let buffer = "";
-    if (req.method == "GET") {
-      console.log("req get", req.method);
-      req.on("data", (data) => {
-        buffer += builder.write(data);
-      });
-
-      req.on("end", () => {
-        let db = new DOA();
-        buffer += builder.end();
-        res.writeHead(200, "OK", headers);
-        console.log("buffer", buffer);
-        db.getVessels(buffer).then((queryData) => {
-          res.end(queryData);
-          console.log("queryData:", queryData);
-        });
-      });
+    switch (req.method) {
+      case "POST":
+        handlePOST(req, res);
+        break;
+      case "GET":
+        handleGET(req, res);
+        break;
+      default:
+        res.end("ONLY ACCEPTS GET AND POST");
     }
-
-    if (req.method == "POST") {
-      console.log("req post", req.method);
-      console.log("req protocol", req.protocol);
-    }
-    //console.log("req", req);
-    console.log("url", req.url);
   })
   .listen(8080, "localhost");
 console.log("Server running");
+
+function handlePOST(req, res) {
+  console.log("req post", req.method);
+  req.on("data", (data) => {
+    console.log("data", JSON.parse(data.toString()));
+  });
+  req.on("end", () => {
+    res.end("received");
+  });
+}
+
+function handleGET(req, res) {
+  let receivedJSON = parseUrl(req.url);
+  console.log("REGEIVED GET");
+  // no clue why you must read this data, but just gotta
+  req.on("data", (data) => {});
+
+  req.on("end", () => {
+    let db = new DOA();
+    res.writeHead(200, "OK", headers);
+    db.getVessels(1).then((results) => {
+      res.end(results);
+    });
+  });
+}
+
+function parseUrl(url) {
+  url = url.replace(/^\/\?/, "");
+  let split = url.split("&");
+  let requestJSON = {};
+  for (let pair of split) {
+    let [key, val] = pair.split("=");
+    requestJSON[key] = val;
+  }
+  return requestJSON;
+}
