adb07a9b563985b6557ba3e70899473ecdc5c5a9
Chandler Clevenger <Chandler888@icloud.com>
2022-04-02 18:14:12 -0400

    make getVessels return all vessels in a tile with a timestamp

--- DOA/DOA.js
+++ DOA/DOA.js
@@ -11,40 +11,34 @@ module.exports = class DOA {
   getTest(echo) {
     return echo;
   }
-  getVessels(tileId) {
+  getVessels(tileId, scale, timestamp) {
     return new Promise((resolve, reject) => {
-      let scale;
       const session = mysqlx.getSession(dbconfigs);
       // we can query the tileId on map_view
       session
         .then((session) => {
-          let map_view = session.getSchema().getTable("map_view");
-          return map_view
-            .select(["scale"])
-            .where("id=:i")
-            .bind("i", tileId)
+          return session
+            .sql(
+              `
+              SELECT *
+              FROM ais_message, position_report
+              WHERE AISMessage_Id = Id
+              AND Timestamp = ?
+              AND MapView` +
+                scale +
+                `_Id = ?
+              `
+            )
+            .bind(timestamp)
+            .bind(tileId)
             .execute();
         })
-        // then we could find all vessels by the tiles in position_report
-        .then((tile) => {
-          scale = tile.fetchAll()[0][0];
-          session
-            .then((session) => {
-              let position_report = session
-                .getSchema()
-                .getTable("position_report");
-              return position_report
-                .select()
-                .where("MapView" + scale + "_Id =:id")
-                .limit(10000)
-                .bind("id", tileId)
-                .execute();
-            })
-            .then((vessels) => {
-              console.log("resolving");
-              resolve(vessels.fetchAll());
-              console.log("After resolve");
-            });
+        .then((vessels) => {
+          console.log("resolving");
+          let v = vessels.fetchAll();
+          resolve(v);
+
+          console.log(v, "After resolve");
         })
         .catch((error) => {
           reject(error);
